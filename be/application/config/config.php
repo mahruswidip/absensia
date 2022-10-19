7189795ffe7604e0c417efc78194e4d569d2794b
mahruswidi <mahrusrama7.1@gmail.com>
2022-10-19 20:11:29 +0700

    Update config.php

--- be/application/config/config.php
+++ be/application/config/config.php
@@ -24,7 +24,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 */
 // $config['base_url'] = 'http://localhost/absensia/be/';
-$config['base_url'] = 'https://alfatihahtravel.com/siswa/be';
+$config['base_url'] = 'https://alfatihahtravel.com/siswa/';
 
 /*
 |--------------------------------------------------------------------------
